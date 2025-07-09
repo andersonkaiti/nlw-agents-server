3a864c3860a1ccb7bcc644b5908477687e48a852
andersonkaiti <anderkaiti@gmail.com>
2025-07-09 12:49:55 -0300

    :sparkles: feat: add get room questions route

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ import {
 } from 'fastify-type-provider-zod'
 import { env } from './env.ts'
 import { createRoomRoute } from './http/routes/create-room.ts'
+import { getRoomQuestionsRoute } from './http/routes/get-room-questions.ts'
 import { getRoomsRoute } from './http/routes/get-rooms.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
@@ -23,7 +24,7 @@ app.get('/health', () => {
 })
 
 app.register(createRoomRoute)
-
 app.register(getRoomsRoute)
+app.register(getRoomQuestionsRoute)
 
 app.listen({ port: env.PORT })
